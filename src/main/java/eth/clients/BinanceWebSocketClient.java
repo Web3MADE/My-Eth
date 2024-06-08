9f2e200fd56f2c1838f6fa63c4e9d8dd4819e3cf
Hakeem <mwo248@yahoo.com>
2024-06-08 15:50:16 +0400

    Updated (WIP) BWC

--- src/main/java/eth/clients/BinanceWebSocketClient.java
+++ src/main/java/eth/clients/BinanceWebSocketClient.java
@@ -1,35 +1,36 @@
 package eth.clients;
 
-import io.smallrye.mutiny.vertx.core.AbstractVerticle;
-import io.vertx.core.http.WebSocketConnectOptions;
+import org.jboss.logging.Logger;
 import io.vertx.mutiny.core.Vertx;
+import io.vertx.mutiny.core.http.HttpClient;
 import io.vertx.mutiny.core.http.WebSocketClient;
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 @ApplicationScoped
-public class BinanceWebSocketClient extends AbstractVerticle {
+public class BinanceWebSocketClient {
+
+    private static final String WEBSOCKET_HOST = "stream.binance.com";
+
+    private static final String WEBSOCKET_URI = "/ws/ethusdt@trade";
     @Inject
     Vertx vertx;
-
     @Inject
-    WebSocketClient webSocketClient;
+    Logger logger;
 
-    @Override
-    public void start() {
-        WebSocketConnectOptions options = new WebSocketConnectOptions()
-                .setURI("wss://stream.testnet.binance.vision/ws/ethusdt@trade");
+    @PostConstruct
+    public void connect() {
+        HttpClient client = vertx.createHttpClient();
 
-        webSocketClient.connect(options).subscribe().with(ws -> ws.handler(buffer -> {
-            String message = buffer.toString();
-            // Broadcast the message to connected WebSocket clients
-            broadcastMessage(message);
-        }), Throwable::printStackTrace);
-
+        WebSocketClient webSocketClient = new WebSocketClient(client);
+        // connect() method established the Websocket connection using the new method
+        webSocketClient.connect(WEBSOCKET_HOST, WEBSOCKET_URI).subscribe()
+                .with(webSocket -> webSocket.handler(buffer -> {
+                    String message = buffer.toString();
+                    System.out.println("Message received " + message);
+                    logger.info("Received message: " + message);
+                }), failure -> logger.error("Failed to connect to WebSocket: ", failure));
     }
 
-    private void broadcastMessage(String message) {
-        // Broadcast the message to all connected WebSocket clients
-        // Implement the logic to send the message to all WebSocket sessions
-    }
 }
