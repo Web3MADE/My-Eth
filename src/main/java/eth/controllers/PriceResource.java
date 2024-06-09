f5034b51dd6da6329ead37f9faef5ede9397cb60
Hakeem <mwo248@yahoo.com>
2024-06-09 12:20:47 +0400

    remove uncessesary endpoint

--- src/main/java/eth/controllers/PriceResource.java
+++ src/main/java/eth/controllers/PriceResource.java
@@ -2,15 +2,17 @@ package eth.controllers;
 
 import eth.services.PriceService;
 import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.core.eventbus.EventBus;
 import jakarta.annotation.security.PermitAll;
-import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 
 @Path("/api/price")
-@ApplicationScoped
 public class PriceResource {
+
+    @Inject
+    EventBus eventBus;
 
     @Inject
     PriceService priceService;
