ed04063a6067f255133560163064030874f9fbf5
Hakeem <mwo248@yahoo.com>
2024-06-09 12:21:42 +0400

    PriceService consumes events - now user price filter

--- src/main/java/eth/services/PriceService.java
+++ src/main/java/eth/services/PriceService.java
@@ -2,6 +2,7 @@ package eth.services;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import eth.interfaces.BinanceClient;
+import io.quarkus.vertx.ConsumeEvent;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -17,6 +18,14 @@ public class PriceService {
         return binanceClient.getPrice("ETHUSDT").onItem().transform(res -> res.price);
     }
 
-
+    // This service automatically consumes price data from WebSocket
+    // Would want a service method to handle user-inputted price filter FIRST and THEN call event
+    // bus
+    // to trigger the notification service
+    @ConsumeEvent("eth-price")
+    public Uni<String> consumeEthPrice(String ethPrice) {
+        System.out.println("eth-price service recieved " + ethPrice);
+        return Uni.createFrom().item(() -> ethPrice.toUpperCase());
+    }
 
 }
